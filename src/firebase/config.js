1230385e3f805e39470af6ed2a99879fc8d5d300
Luka Tamazashvili <lukatamaza@gmail.com>
2025-10-24 00:12:00 +0400

    Change Firebase config to use standard environment variables
    Updated Firebase configuration to use non-VITE environment variables.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,8 +4,8 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
